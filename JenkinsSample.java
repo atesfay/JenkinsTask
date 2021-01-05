531adea5a60944d77d7a9a596f3479a14d58e92d
atesfay <abrahamkt@yahoo.com>
2021-01-05 14:46:46 -0800

    added JenkinsSample program

---
+++ JenkinsSample.java
@@ -0,0 +1,8 @@
+public class JenkinsSample{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Task..." + i + " run successfuly!");
+        }
+        System.out.println("Program Terminated!");
+    }
+}
